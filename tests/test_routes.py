b2d7065cd61556762a263ee69e28300cdc37a89f
allatassemmaxwell <allarassemmaxwell@gmail.com>
2024-08-28 16:00:49 -0400

    Fix linting issues: remove trailing whitespace and add newline

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -127,7 +127,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
-    def test_read_an_account(self): 
+    def test_read_an_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
